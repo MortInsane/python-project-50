a67018fde29f9a04406049974c26136fdb55ded1
Mort Insane <mort.insane@gmail.com>
2022-12-04 04:47:31 +0300

    add cli

---
+++ gendiff/cli.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files ans shows a difference.'
+    )
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+
+    return parser.parse_args()
