7f61ddaed15a876cc7ea12006e61d73d3036c1f1
Mort Insane <mort.insane@gmail.com>
2022-12-22 07:18:50 +0300

    rm tools

--- gendiff/tools.py
+++
@@ -1,9 +0,0 @@
-def normalize_data(value):
-    if type(value) == bool and value:
-        return 'true'
-    elif type(value) == bool and not value:
-        return 'false'
-    elif value is None:
-        return 'null'
-    else:
-        return value
